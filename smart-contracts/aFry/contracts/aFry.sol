9786187bed9f103ab0ca4d35f6807b06bdda60b1
Laderlappen <elmervermeulen@gmail.com>
2021-11-29 11:52:18 +0200

    Added mint and burn functions and mande token ownable

--- smart-contracts/aFry/contracts/aFry.sol
+++ smart-contracts/aFry/contracts/aFry.sol
@@ -3,7 +3,17 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract aFRY is ERC20 {
+contract aFRY is ERC20, Ownable {
   constructor() ERC20("Arbitrum FRY", "aFRY") {}
+
+  function mint(address recipient, uint256 amount) public onlyOwner {
+    _mint(recipient, amount);
+  }
+
+  function burn(address recipient, uint256 amount) public {
+    _burn(recipient, amount);
+  }
 }
--- smart-contracts/aFry/test/aFryTests.js
+++ smart-contracts/aFry/test/aFryTests.js
@@ -1,9 +1,26 @@
 const aFry = artifacts.require("aFRY");
 
-contract("aFry", () => {
+contract("aFry", (accounts) => {
   it("Initial supply should be zero", async () => {
     const aFryContract = await aFry.new();
     const supply = await aFryContract.totalSupply();
     assert(supply.toString() === "0");
+  });
+
+  it("Should be able to mint", async () => {
+    const aFryContract = await aFry.new();
+    await aFryContract.mint(accounts[0], "100");
+    const supply = await aFryContract.totalSupply();
+    //console.log("Total Supply: ", supply);
+    assert(supply.toString() === "100");
+  });
+
+  it("Should be able to burn", async () => {
+    const aFryContract = await aFry.new();
+    await aFryContract.mint(accounts[0], "100");
+    await aFryContract.burn(accounts[0], "50");
+    const supply = await aFryContract.totalSupply();
+    //console.log("Total Supply: ", supply);
+    assert(supply.toString() === "50");
   });
 });
