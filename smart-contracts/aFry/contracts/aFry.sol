733542da565fb95b64660098b2efd6cc46feffd8
Laderlappen <elmervermeulen@gmail.com>
2021-11-29 16:16:23 +0200

    Use SafeMath

--- smart-contracts/aFry/contracts/aFry.sol
+++ smart-contracts/aFry/contracts/aFry.sol
@@ -8,5 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/GSN/Context.sol";
 
 contract aFRY is Context, ERC20Detailed, ERC20Mintable, ERC20Burnable {
+  using SafeMath for uint256;
+
   constructor() public ERC20Detailed("Arbitrum FRY", "aFRY", 18) {}
 }
