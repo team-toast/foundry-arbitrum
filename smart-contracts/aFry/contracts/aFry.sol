219529f254ebac46c1e42ea0bc6719955d893464
Laderlappen <elmervermeulen@gmail.com>
2021-11-29 14:54:22 +0200

    Changed parameter names

--- smart-contracts/aFry/contracts/aFry.sol
+++ smart-contracts/aFry/contracts/aFry.sol
@@ -8,11 +8,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract aFRY is ERC20, Ownable {
   constructor() ERC20("Arbitrum FRY", "aFRY") {}
 
-  function mint(address recipient, uint256 amount) public onlyOwner {
-    _mint(recipient, amount);
+  function mint(address account, uint256 amount) public onlyOwner {
+    _mint(account, amount);
   }
 
-  function burn(address recipient, uint256 amount) public {
-    _burn(recipient, amount);
+  function burn(address account, uint256 amount) public {
+    _burn(account, amount);
   }
 }
